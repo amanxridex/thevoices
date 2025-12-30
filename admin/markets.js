7a295925d4d476c4d4504c45a15c5d6cf7d510e9
Aman Mishra <thegreatestaman999@gmail.com>
2025-12-30 13:16:01 +0530

    new

---
+++ admin/markets.js
@@ -0,0 +1,22 @@
+let currentMarketId = null;
+
+function editMarket(id) {
+  currentMarketId = id;
+  document.getElementById("editModal").style.display = "block";
+}
+
+function saveMarket() {
+  fetch(API + "/admin/market/" + currentMarketId, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + localStorage.getItem("adminToken")
+    },
+    body: JSON.stringify({
+      name: editName.value,
+      openAt: editOpen.value,
+      closeAt: editClose.value
+    })
+  })
+  .then(() => location.reload());
+}
